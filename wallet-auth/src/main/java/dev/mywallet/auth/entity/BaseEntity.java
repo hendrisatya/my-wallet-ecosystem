263e1394db59a27197cf0015a91369c2f3fca9b7
hendrisatya <nugraha.hendrisatya@protonmail.com>
2026-02-08 17:53:31 +0700

    created base entity fro auth entities

---
+++ wallet-auth/src/main/java/dev/mywallet/auth/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package dev.mywallet.auth.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
