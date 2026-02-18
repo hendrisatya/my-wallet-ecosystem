e4744486bf3ebd2ddbe3dfa41eb695bd140d7916
hendrisatya <nugraha.hendrisatya@gmail.com>
2026-02-18 15:30:56 +0700

    created wallet gateway service

---
+++ wallet-gateway/src/main/java/dev/mywallet/gateway/WalletGatewayApplication.java
@@ -0,0 +1,11 @@
+package dev.mywallet.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletGatewayApplication.class, args);
+    }
+}
